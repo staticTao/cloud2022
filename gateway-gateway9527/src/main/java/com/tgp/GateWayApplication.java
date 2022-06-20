79a8ba6ae5a6969a150e9e02eb2e787d65a84c12
TGP <1227470298@qq.com>
2022-06-20 09:34:15 +0800

    使用Hystrix的fellback (服务降级)

---
+++ gateway-gateway9527/src/main/java/com/tgp/GateWayApplication.java
@@ -0,0 +1,13 @@
+package com.tgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
